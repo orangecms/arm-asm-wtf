99eb3b9c735bef85e2d8b93bbf4e54b64a77ec21
Daniel Maslowski <info@orangecms.org>
2025-06-23 11:37:37 +0200

    picking a config

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,25 @@ use unicorn_engine::RegisterARM64;
 use unicorn_engine::unicorn_const::{Arch, Mode, Permission, SECOND_SCALE};
 
 // https://shell-storm.org/online/Online-Assembler-and-Disassembler/
-const CODE: [u8; 8] = [
-    0x64, 0x1f, 0x00, 0x51, // sub w4, w27, #0x7
-    0x9f, 0x04, 0x00, 0x71, // cmp w4, #0x1
+const CODE: [u8; 4] = [
+    0x60, 0x5a, 0x75, 0xf8, // ldr   x0, [x19, w21, uxtw #3]
 ];
 const CODE_SIZE: u64 = CODE.len() as u64;
 
-const MEM_BASE: u64 = 0x1000;
+const MEM_BASE: u64 = 0xfdcc_8000;
 const MEM_SIZE: usize = 0x4000;
 
 const TIMEOUT: u64 = 10 * SECOND_SCALE;
 const MAX_INSTRUCTIONS: usize = 1000;
+
+const DATA: [u8; 32] = [
+    0x88, 0xba, 0xcc, 0xfd, 0x00, 0x00, 0x00, 0x00, // cfg slot 0
+    0x68, 0xa8, 0xcc, 0xfd, 0x00, 0x00, 0x00, 0x00, // cfg slot 1
+    0xb8, 0x9b, 0xcc, 0xfd, 0x00, 0x00, 0x00, 0x00, // cfg slot 2
+    0x98, 0x89, 0xcc, 0xfd, 0x00, 0x00, 0x00, 0x00, // cfg slot 3
+];
+
+const CFG_BASE: u64 = 0xfdcc8940;
 
 fn main() {
     let mut emu = unicorn_engine::Unicorn::new(Arch::ARM64, Mode::LITTLE_ENDIAN)
@@ -22,24 +30,20 @@ fn main() {
     emu.mem_write(MEM_BASE, &CODE)
         .expect("failed to write instructions");
 
-    emu.reg_write(RegisterARM64::W27, 0x3)
-        .expect("failed write W27");
+    emu.mem_write(CFG_BASE, &DATA)
+        .expect("failed to write data");
+
+    emu.reg_write(RegisterARM64::X19, CFG_BASE)
+        .expect("failed to write X19");
+    emu.reg_write(RegisterARM64::W21, 0x3)
+        .expect("failed to write W21");
 
     emu.emu_start(MEM_BASE, MEM_BASE + CODE_SIZE, TIMEOUT, MAX_INSTRUCTIONS)
         .unwrap();
 
-    // flags are stored in the highest 4 bits
-    // https://developer.arm.com/documentation/ddi0601/2025-03/AArch64-Registers/NZCV--Condition-Flags
-    let r = emu.reg_read(RegisterARM64::NZCV).unwrap() >> 28;
+    let r = emu.reg_read(RegisterARM64::W21).unwrap();
+    println!("w21:   {r:08x}");
 
-    // The result determines conditional branch behavior.
-    // E.g., C = 1 and Z = 0 will branch on B.HI (unsigned greater than).
-    // https://devblogs.microsoft.com/oldnewthing/20220815-00/?p=106975
-    println!("NZCV: {r:04b}"); // 1010
-
-    let r = emu.reg_read(RegisterARM64::W4).unwrap();
-    println!("W4:   {r:08x}");
-
-    let r = emu.reg_read(RegisterARM64::W27).unwrap();
-    println!("W27:  {r:08x}");
+    let r = emu.reg_read(RegisterARM64::X0).unwrap();
+    println!("res:   {r:08x}");
 }
